58251bb77065956825f582a332ef877ad9b348af
Justus Post <JPost@lcs.on.ca>
2019-11-20 11:32:54 -0500

    Hooked up the view to the controller (Outlets and Actions created), Also added an output Label

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -10,24 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Mark: Properties
+    // Outlets
+    @IBOutlet weak var phraseInputfield: UITextField!
     
+    @IBOutlet weak var outputResult: UILabel!
+    
+    
+    // Mark: Methods
+    // Functions (Makes Things happen)
+    // Actions go here
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
-        
     }
 
-// Properties
-    @IBOutlet weak var TextInput: UITextField!
-
-        
-    }
-
-
-func Analyze(_ sender: Any) {
+    // Analyze the text provided
+    @IBAction func analyzeText(_ sender: Any) {
     }
     
+        
+    }
+
+
 
 
